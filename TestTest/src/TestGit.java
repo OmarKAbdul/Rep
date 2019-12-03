6d96d25d575d3a08cdd982f24aafa978ee56e6a7
OmarKAbdul <OmarKhan0727@gmail.com>
2019-12-02 21:05:30 -0500

    hello

---
+++ TestTest/src/TestGit.java
@@ -0,0 +1,11 @@
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Git Practice");
+		System.out.println("Hello");
+		System.out.println("Welcome Back");
+	}
+
+}
